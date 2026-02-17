994307da5502bab3c9f97c542733c1d31951d16a
Edmilson Rodrigues <ed.ufpe@gmail.com>
2026-02-17 10:18:34 -0300

    feat: Add Next.js middleware configured for the root path.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,8 @@
-import { type NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-    return Response.next()
+    return NextResponse.next()
 }
 
 export const config = {
