77423d85fc96c60734f541cc4d11fdf0725766f1
Edmilson Rodrigues <ed.ufpe@gmail.com>
2026-02-17 16:39:08 -0300

    Add standalone output config for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: 'standalone',
+  distDir: '.next',
+};
 
 export default nextConfig;
